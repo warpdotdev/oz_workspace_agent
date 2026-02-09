ad9fec12730ee4b6febd94efb2b920c2d700ba86
David Plakon <dplakon@gmail.com>
2026-02-08 22:52:03 -0600

    removed stuff

--- agent_mesh/registry/__init__.py
+++
@@ -1,7 +0,0 @@
-"""AgentMesh Registry - Agent discovery and message routing."""
-try:
-    from agent_mesh.registry.registry import AgentRegistry, DistributedRegistry
-except ImportError:
-    from .registry import AgentRegistry, DistributedRegistry
-
-__all__ = ["AgentRegistry", "DistributedRegistry"]
